770b8e76332c274fe96c6d911b0f070fc60c8f78
emirsurmen <63517174+emirsurmen@users.noreply.github.com>
2020-11-16 12:23:24 +0300

    Added a LOT more functions to country.py

--- corona-python/country.py
+++ corona-python/country.py
@@ -56,3 +56,129 @@ class Country:
         else:
             return corona["deaths"]
 
+    def today_deaths(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["todayDeaths"]
+
+    def recovered(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["recovered"]
+
+    def today_recovered(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["todayRecovered"]
+
+    def active(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["active"]
+
+    def critical(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["deaths"]
+
+    def cases_per_one_million(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["casesPerOneMillion"]
+
+    def deaths_per_one_million(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["deathsPerOneMillion"]
+
+    def total_tests(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["tests"]
+
+    def tests_per_one_million(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["testsPerOneMillion"]
+
+    def population(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["population"]
+
+    def continent(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["continent"]
+
+    def one_case_per_people(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["oneCasePerPeople"]
+
+    def one_death_per_people(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["oneDeathPerPeople"]
+
+    def one_test_per_people(self):
+        request = requests.get(f"https://disease.sh/v2/countries/{self.country.replace(' ', '%20')}")
+        corona = json.loads(request.content)
+
+        if request.status_code != 200 and request.status_code != 404:
+            return None
+        else:
+            return corona["oneTestPerPeople"]
+
