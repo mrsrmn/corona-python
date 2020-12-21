0836024e1e80785271030d7175314dcaa4307788
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2020-12-21 13:16:38 +0300

    Changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="corona_python",
-    version="1.0.1",
+    version="1.1.0",
     author="MakufonSkifto",
     author_email="emirsurmen@gmail.com",
     license="MIT",
